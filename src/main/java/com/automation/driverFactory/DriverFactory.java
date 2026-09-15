704349730e816e2efbf2d19f4bd69cfa353bf9b4
just2code <just2code19@gmail.com>
2026-09-15 11:00:12 +0530

    updated headless

--- src/main/java/com/automation/driverFactory/DriverFactory.java
+++ src/main/java/com/automation/driverFactory/DriverFactory.java
@@ -21,8 +21,10 @@ public class DriverFactory {
 		WebDriverManager.chromedriver().setup();
 		ChromeOptions options = new ChromeOptions();
 		//options.addArguments("--start-maximized");
-		//options.addArguments("--headless=new");
+		options.addArguments("--headless=new");
 		options.addArguments("--window-size=1920,1080");
+		options.addArguments("--disable-gpu");
+		options.addArguments("--no-sandbox");
 		WebDriver driver = new ChromeDriver(options);
 		driverThreadLocal.set(driver);
 	}
